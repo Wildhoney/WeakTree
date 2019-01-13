ebfbb1d822c5ebe3afe50e37e8b7fda553311a7a
Wildhoney <adam.timberlake@gmail.com>
2019-01-13 18:34:50 +0000

    Passing map to each helper function and recursing only on the keys

--- src/index.js
+++ src/index.js
@@ -1,43 +1,48 @@
-const set = function recurse(map, [key, ...keys], value) {
-    const isLast = keys.length === 0;
-    const isValid = map.has(key);
-    const result =
-        (!isValid || isLast) && map.set(key, isLast ? value : new WeakMap());
-    return isLast ? result : recurse(map.get(key), keys, value);
+const set = function recurse(map) {
+    return ([key, ...keys], value) => {
+        const isLast = keys.length === 0;
+        const isValid = map.has(key);
+        const result =
+            (!isValid || isLast) &&
+            map.set(key, isLast ? value : new WeakMap());
+        return isLast ? result : recurse(map.get(key))(keys, value);
+    };
 };
 
 const get = f =>
-    function recurse(map, [key, ...keys]) {
-        const isLast = keys.length === 0;
-        const isValid = map.has(key);
-        return isLast || !isValid
-            ? f.call(map, key)
-            : recurse(map.get(key), keys);
+    function recurse(map) {
+        return ([key, ...keys]) => {
+            const isLast = keys.length === 0;
+            const isValid = map.has(key);
+            return isLast || !isValid
+                ? f.call(map, key)
+                : recurse(map.get(key))(keys);
+        };
     };
 
 const isWeakMap = a => a instanceof WeakMap;
 
+const isIterable = a => a && typeof a[Symbol.iterator] === 'function';
+
 export default function WeakTree(data) {
     const map = isWeakMap(data) ? data : new WeakMap();
 
-    const helpers = {
-        set: set,
-        get: get(map.get),
-        has: get(map.has),
-        delete: get(map.delete)
+    const f = {
+        set: set(map),
+        get: get(map.get)(map),
+        has: get(map.has)(map),
+        delete: get(map.delete)(map)
     };
 
-    const isIterable = data && typeof data[Symbol.iterator] === 'function';
-    isIterable &&
-        data.forEach(([keys, value]) => helpers.set(map, keys, value));
+    isIterable(data) && data.forEach(([keys, value]) => f.set(keys, value));
 
     return {
-        set: (keys, value) => helpers.set(map, keys, value),
-        get: keys => helpers.get(map, keys),
-        has: keys => helpers.has(map, keys),
-        delete: keys => helpers.delete(map, keys),
+        set: f.set,
+        get: f.get,
+        has: f.has,
+        delete: f.delete,
         slice: keys => {
-            const value = helpers.get(map, keys);
+            const value = f.get(keys);
             return isWeakMap(value) ? new WeakTree(value) : value;
         }
     };
